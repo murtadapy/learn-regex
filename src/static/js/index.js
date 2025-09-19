cc95e7d0d890cbd93ac34adf082da617081e4810
Murtada Altarouti <murtada.altarouti@gmail.com>
2025-09-19 14:17:03 +0300

    Added Color List

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,6 +1,14 @@
 const regexInput = document.getElementById("regex-input");
 const content = document.getElementById("content");
 let originalText = content.innerText;
+const colorList = [
+    "#167ccc",
+    "#17CB44",
+    "#DAB42B",
+    "#CC1616",
+    "#Cc8616",
+    "#C616cc",
+];
 
 function reset() {
     content.innerText = originalText;
@@ -12,8 +20,11 @@ function handleRegexInputChange() {
     try {
         if (regexInput.value) {
             const regex = new RegExp(regexInput.value, "g");
+            let matchIndex = 0;
             const highlighted = originalText.replaceAll(regex, (match) => {
-                return `<span style="color: #1a1a1a; background: #17CB44;">${match}</span>`;
+                const color = colorList[matchIndex % colorList.length];
+                matchIndex++;
+                return `<span style="color: #1a1a1a; background: ${color};">${match}</span>`;
             });
             content.innerHTML = highlighted;
         }
